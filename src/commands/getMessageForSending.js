d7b6a5a4d5f2521c1649d150522929239e3661c2
Xedll <97981835+Xedll@users.noreply.github.com>
2024-01-30 15:26:38 +0300

    v3.1
    Починено добавление #

--- src/commands/getMessageForSending.js
+++ src/commands/getMessageForSending.js
@@ -1,11 +1,12 @@
 const getMessageForSending = (site, post) => {
    const postDate = new Date(post.date * 1000)
+   const siteName = site.name
    const postMonth = postDate.getMonth() + 1 >= 10 ? postDate.getMonth() + 1 : '0' + (postDate.getMonth() + 1)
    const postDay = postDate.getDate() >= 10 ? postDate.getDate() : '0' + (postDate.getDate())
    const postHours = postDate.getHours() >= 10 ? postDate.getHours() : '0' + (postDate.getHours())
    const postMinutes = postDate.getMinutes() >= 10 ? postDate.getMinutes() : '0' + (postDate.getMinutes())
    const postSeconds = postDate.getSeconds() >= 10 ? postDate.getSeconds() : '0' + (postDate.getSeconds())
-   let message = `<a href="${site.domain}">${site.name}</a> (<a href="${site.domain}?w=wall${site.owner_id}_${post.id}">${postDay}.${postMonth}.${postDate.getFullYear()} ${postHours}:${postMinutes}:${postSeconds}</a>) #${site.name.split(' ').join('_')}\n\n${post.text}`
+   let message = `<a href="${site.domain}">${site.name}</a> (<a href="${site.domain}?w=wall${site.owner_id}_${post.id}">${postDay}.${postMonth}.${postDate.getFullYear()} ${postHours}:${postMinutes}:${postSeconds}</a>) #${siteName.split(' ').join('_')}\n\n${post.text}`
    return message
 }
 module.exports = getMessageForSending;
